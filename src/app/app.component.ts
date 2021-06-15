f734993f53a7403b5d32080cdf2db2990cbc3249
Julian Saenze <julian.saenze@comsysto.com>
2021-06-15 09:46:46 +0200

    outsourcing logic in a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -11,58 +11,28 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
-  url: string = 'https://ng-http-start-aa1e8-default-rtdb.europe-west1.firebasedatabase.app/posts.json';
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private postsService: PostsService) {}
+
   ngOnInit() {
     //whenever this app/page loads -> get the data
-    this.fetchPosts();
+    this.postsService.fetchPost();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
-    //store data args (url/request body(data to store - normally json data))
-    //Requests are only set if subscribed
-    this.http
-    .post<{name: string}>(
-      this.url,
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.postsService.fetchPost();
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  //transform data with pipe
-  //map operator - allow to get some data and return new data which is rewrapped into an observable
-  //tell which type of object the retrieved data is -> <{[key: string]: Post}>
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http
-      .get<{[key: string]: Post}>(this.url)
-      .pipe(map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)){
-            //push a new object in the array / spread operator will pull out all key value pairs of the nested object
-            postsArray.push({...responseData[key], id: key})
-          }
-        }
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
-  }
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from 'rxjs/operators';
+
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+  constructor(private http: HttpClient){}
+
+  url: string = 'https://ng-http-start-aa1e8-default-rtdb.europe-west1.firebasedatabase.app/posts.json';
+
+  createAndStorePost(title: string, content: string){
+    //send http request
+    //store data args (url/request body(data to store - normally json data))
+    //Requests are only set if subscribed
+    const postData: Post = {title: title, content: content};
+    this.http
+    .post<{name: string}>(
+      this.url,
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  //transform data with pipe
+  //map operator - allow to get some data and return new data which is rewrapped into an observable
+  //tell which type of object the retrieved data is -> <{[key: string]: Post}>
+  fetchPost(){
+    this.http
+      .get<{[key: string]: Post}>(this.url)
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)){
+            //push a new object in the array / spread operator will pull out all key value pairs of the nested object
+            postsArray.push({...responseData[key], id: key})
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+
+      });
+  }
+}
