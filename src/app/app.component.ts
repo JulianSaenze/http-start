b0f2372384bf092d2b1e17260ea0534f0c4ffa27
Julian Saenze <julian.saenze@comsysto.com>
2021-06-15 09:23:19 +0200

    sending post / getting data / transform data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +11,55 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  url: string = 'https://ng-http-start-aa1e8-default-rtdb.europe-west1.firebasedatabase.app/posts.json';
 
   constructor(private http: HttpClient) {}
+  ngOnInit() {
+    //whenever this app/page loads -> get the data
+    this.fetchPosts();
+  }
 
-  ngOnInit() {}
-
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     console.log(postData);
+    //store data args (url/request body(data to store - normally json data))
+    //Requests are only set if subscribed
+    this.http
+    .post<{name: string}>(
+      this.url,
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  //transform data with pipe
+  //map operator - allow to get some data and return new data which is rewrapped into an observable
+  //tell which type of object the retrieved data is -> <{[key: string]: Post}>
+  private fetchPosts() {
+    this.http
+      .get<{[key: string]: Post}>(this.url)
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)){
+            //push a new object in the array / spread operator will pull out all key value pairs of the nested object
+            postsArray.push({...responseData[key], id: key})
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
